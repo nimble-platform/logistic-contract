ab1a8cc7a0935dd996ae33f359e63b41943f68b1
Akayeshmantha <akayeshmantha@apache.org>
2020-09-03 11:32:08 +0200

    logger fixes.

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -11,7 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import winston from "winston";
+import * as winston from 'winston';
 
 const options: winston.LoggerOptions = {
     transports: [
