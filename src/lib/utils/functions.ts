fb835578e7d196f59ce091a929194a6dda13225e
ayeshmantha.perera <ayeshmantha.perera@salzburgresearch.at>
2020-11-24 18:38:28 +0100

    add generic type converter.

--- src/lib/utils/functions.ts
+++ src/lib/utils/functions.ts
@@ -17,3 +17,23 @@ import { NetworkNameUUID } from '../../constants';
 export function generateId(txId: string, ...additionalData: string[]) {
     return uuid(txId + ':' + additionalData.join(':'), NetworkNameUUID);
 }
+
+type Descriptor<T> = {
+    [P in keyof T]: (v: any) => T[P];
+};
+
+export function pick<T>(v: any, d: Descriptor<T>): T {
+    const ret: any = {};
+    for (let key in d) {
+        try {
+            const val = d[key](v[key]);
+            if (typeof val !== 'undefined') {
+                ret[key] = val;
+            }
+        } catch (err) {
+            const msg = err instanceof Error ? err.message : String(err);
+            throw new Error(`could not pick ${key}: ${msg}`);
+        }
+    }
+    return ret;
+}
